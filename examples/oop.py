c64e2393c49d83e6d795e55ceb1b06c4d1292193
Iulia Chiriac <iulia.chiriac@yahoo.com>
2024-12-04 12:41:15 +0200

    Add Car class inheriting Vehicle

--- examples/oop.py
+++ examples/oop.py
@@ -4,7 +4,7 @@ import datetime
 
 class Vehicle:
     # Class attribute
-    wheels = 4
+    wheels = 0
 
     def __init__(self, color, brand, model=None):
         # print("inside __init__", self)
@@ -28,6 +28,21 @@ class Vehicle:
         print("Current speed:", self.speed)
 
 
+class Car(Vehicle):
+    wheels = 4
+
+    def __init__(self, color, make, model, fuel_type):
+        super().__init__(color, make, model)
+        self.fuel_type = fuel_type
+
+    def display_speed(self):
+        print(f"I am {self.make} {self.model}")
+        super().display_speed()
+
+    def honk_horn(self):
+        print("Beep beep!")
+
+
 if __name__ == "__main__":
     # help(datetime)
     # print(dir(datetime))
@@ -37,28 +52,36 @@ if __name__ == "__main__":
     print(dt.year, datetime.date.max, dt.max)
     # dt.year = 2001
 
-    my_car = Vehicle(color="green", brand="Dacia", model="Sandero")  # Vehicle.__init__
-    your_car = Vehicle("white", "Audi")
+    my_vehicle = Vehicle(color="green", brand="Dacia", model="Sandero")
+    your_vehicle = Vehicle("white", "Audi")
 
-    print(dir(my_car))
-    print("Accessing object attributes:", my_car.color, my_car.wheels, my_car.speed)
+    print(dir(my_vehicle))
+    print("Accessing object attributes:", my_vehicle.color, my_vehicle.wheels,
+          my_vehicle.speed)
 
-    print(your_car.make, your_car.speed, your_car.model)
-    your_car.speed += 20
-    your_car.model = "Q3"
-    print(your_car.make, your_car.speed, your_car.model)
+    print(your_vehicle.make, your_vehicle.speed, your_vehicle.model)
+    your_vehicle.speed += 20
+    your_vehicle.model = "Q3"
+    print(your_vehicle.make, your_vehicle.speed, your_vehicle.model)
 
     # Vehicle.wheels = 7
     print("Class attribute value:", Vehicle.wheels)
     # print(my_car, your_car)
 
-    my_car.display_speed()
-    my_car.accelerate(20)
-    my_car.display_speed()
-    my_car.brake(15)
-    my_car.display_speed()
+    my_vehicle.display_speed()
+    my_vehicle.accelerate(20)
+    my_vehicle.display_speed()
+    my_vehicle.brake(15)
+    my_vehicle.display_speed()
 
     try:
-        my_car.accelerate(-1)
+        my_vehicle.accelerate(-1)
     except ValueError as ex:
         print(ex)
+
+    my_car = Car("white", "Dacia", "Logan", "gasoline")
+    print(type(my_car))
+    print(my_car.fuel_type)
+    my_car.accelerate(100)
+    my_car.display_speed()
+    my_car.honk_horn()
